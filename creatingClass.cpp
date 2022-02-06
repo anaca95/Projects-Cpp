e1bc58601215e94981854e46bde7db82c856dec9
Ana Carolina Gonçalves <81190988+anaca95@users.noreply.github.com>
2022-02-06 13:38:47 +0100

    File added to the repo

---
+++ creatingClass.cpp
@@ -0,0 +1,31 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string>
+#include <iostream>
+using namespace std;
+
+class myClass{
+    public: //define that methods and attributes are visible outside the class
+        int myNumber;
+        std::string myString;
+
+};
+
+int main(){
+    myClass myObj; //Create an object of class myClass
+    
+    // Access attributes and set values
+    myObj.myNumber = 15;
+    myObj.myString = "hello";
+
+    //Print attribute values
+   std:: cout << myObj.myNumber << "\n";
+   std:: cout <<myObj.myString;
+
+   // Or print using printf:
+   //printf("%d",myObj.myNumber);
+   //printf("%s", myObj.myString);
+
+   
+    return 0;
+}
