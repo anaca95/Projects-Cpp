74d589c10533496934bc78109f7401311d4bd6fe
Ana <anaca2795@gmail.com>
2022-05-15 21:26:53 +0200

    Replace input syntax and add class user

--- loginForm.cpp
+++ loginForm.cpp
@@ -1,24 +1,45 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
+#include <iostream>
+using namespace std;  //when using strings
+
+class user {
+  public:
+    string name;   
+    string surname;
+    string sex;
+    int age;
+    int plz;
+};
 
 int main(){
+    // Create an object of user
+    user user_1;
+    std::string username;
+    std::string password;
     char nome[8]={0};
     char senha[8]={0};
     printf("Novo Cadastro\n\n");
-    printf("informe o nome usuario [max 8 caracteres/numeros]:\n");
-    gets(nome);
-    printf("informe a senha [max 8 caracteres/numeros]:\n");
-    gets(senha);
+    std::cout <<("informe o nome usuario [max 8 caracteres/numeros]:\n");
+    std::cin >> username;
+    user_1.name = username;
+    
+    std::cout << ("informe a senha [max 8 caracteres/numeros]:\n");
+    std::cin >> password;
+    user_1.name = password;
+    printf(username.c_str(),'\n');
+    printf(password.c_str());
+    
 
-            if(strlen(nome)<=8 && nome!=NULL)
-        {
-            if(strlen(senha)<=8 && senha!=NULL){
-            printf("\n >> Cadastro realizado com sucesso!");}
-            else
-            printf("\n >> Cadastro falhou! Verifique a senha.");
-        }
-        else 
-            printf("Cadastro falhou! Verifique o usuario.");
+           // if(strlen(username)<=8 && nome!=NULL)
+       // {
+       //     if(strlen(password)<=8 && senha!=NULL){
+       //     printf("\n >> Cadastro realizado com sucesso!");}
+       //     else
+       //     printf("\n >> Cadastro falhou! Verifique a senha.");
+       // }
+        //else 
+          //  printf("Cadastro falhou! Verifique o usuario.");
 return(0);
 }
